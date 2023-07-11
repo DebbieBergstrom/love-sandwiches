1e69fbe9a3cee9aef81606e5e9304396cac03362
Debbie Bergstrom <debbiebergstroem@gmail.com>
2023-07-11 09:48:16 +0000

    Add calculated surplus data to surplus worksheet

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
@@ -87,6 +84,15 @@ def calculate_surplus_data(sales_row):
  
     return surplus_data
 
+def update_surplus_worksheet(data):
+    """ 
+    Update surplus worksheet, add ner row with the list of data provided.
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully.\n")
+
 def main():
     """
     Run all program functions
@@ -95,7 +101,8 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
+   
 
 print("Welcome to Love Sandwich Data Automation!")
 main()
